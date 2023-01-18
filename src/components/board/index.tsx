2208a99944ec9feeb89ff5f34921406045d95931
Frederico Costa <costafredericob@gmail.com>
2023-01-17 21:38:39 -0300

    feat/board - base do componente concluida

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -1,5 +1,27 @@
 function Board() {
-  return <div className="app__calculator--board">Board</div>;
+  return (
+    <div className="app__calculator--board">
+      <div className="board__title--wrapper">
+        <h2>VOCÊ RECEBERÁ:</h2>
+      </div>
+      <div className="board__list--wrapper">
+        <ul>
+          <li>
+            Amanhã: <span>R$ 0,00</span>
+          </li>
+          <li>
+            Em 15 dias: <span>R$ 0,00</span>
+          </li>
+          <li>
+            Em 30 dias: <span>R$ 0,00</span>
+          </li>
+          <li>
+            Em 90 dias: <span>R$ 0,00</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Board;
